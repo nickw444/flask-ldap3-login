db40d95a8eed8ff7dbed760de52094d75843ff80
Nick Whyte <nick@nickwhyte.com>
2021-07-25 14:21:14 +1000

    Release version

--- setup.py
+++ setup.py
@@ -2,28 +2,13 @@ import os
 
 from setuptools import setup
 
-
-def get_version():
-    version_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "VERSION")
-    v = open(version_path).read()
-    if type(v) == str:
-        return v.strip()
-    return v.decode("UTF-8").strip()
-
-
 readme_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "README.rst")
 long_description = open(readme_path).read()
-
-try:
-    version = get_version()
-except Exception:
-    version = "0.0.0-dev"
 
 requires = ["ldap3>=2.0.7", "Flask", "Flask-wtf"]
 
 setup(
     name="flask-ldap3-login",
-    version=version,
     packages=["flask_ldap3_login"],
     author="Nick Whyte",
     author_email="nick@nickwhyte.com",
